b1c1df007536f050d30558a776ba15855bf5bb01
Tomas Vymazal <tomas.vymazal@gmail.com>
2016-12-05 12:49:34 +0100

    Empty screen rendering added.

--- Glitter/Glitter/Headers/glitter.hpp
+++ Glitter/Glitter/Headers/glitter.hpp
@@ -3,12 +3,15 @@
 #define GLITTER
 #pragma once
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+
 // System Headers
 #include <assimp/Importer.hpp>
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
 #include <btBulletDynamicsCommon.h>
-#include <glad/glad.h>
+//#include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
---
+++ Glitter/Glitter/Sources/main.cpp
@@ -0,0 +1,72 @@
+// Local Headers
+#include "glitter.hpp"
+#include <iostream>
+
+// System Headers
+//#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // When a user presses the escape key, we set the WindowShouldClose property to true,
+    // closing the application
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main(int argc, char * argv[]) {
+
+    // Load GLFW and Create a Window
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    auto window = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+
+    // Check for Valid Context
+    if (window == nullptr) {
+        fprintf(stderr, "Failed to Create OpenGL Context");
+        return EXIT_FAILURE;
+    }
+
+    // Create Context and Load OpenGL Functions
+    glfwMakeContextCurrent(window);
+    //gladLoadGL();
+    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+    
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+    
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    
+    glfwSetKeyCallback(window, key_callback);
+
+    // Rendering Loop
+    while (glfwWindowShouldClose(window) == false)
+    {
+        glfwPollEvents();
+        
+        // Background Fill Color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Flip Buffers and Draw
+        glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
+    
+    return EXIT_SUCCESS;
+}
