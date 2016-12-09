e32ce594173b2077add48f588bde415434a28051
Tomas Vymazal <tomas.vymazal@gmail.com>
2016-12-09 12:30:25 +0100

    transformed quad lying on the plane

--- Glitter/Glitter/Sources/main.cpp
+++ Glitter/Glitter/Sources/main.cpp
@@ -7,6 +7,9 @@
 //#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <SOIL.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // Standard Headers
 #include <cstdio>
@@ -140,6 +143,15 @@ int main(int argc, char * argv[]) {
     // Rendering Loop
     while (glfwWindowShouldClose(window) == false)
     {
+        glm::mat4 model;
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        
+        glm::mat4 view;
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        
+        glm::mat4 projection;
+        projection = glm::perspective((GLfloat)glm::radians(45.0), width / (GLfloat) height, 0.1f, 100.0f);
+        
         glfwPollEvents();
         
         // Background Fill Color
@@ -152,6 +164,9 @@ int main(int argc, char * argv[]) {
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
         glUniform1i(glGetUniformLocation(shader.Program, "ourTexture2"), 1);
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         
         shader.Use();
         glBindVertexArray(VAO);
